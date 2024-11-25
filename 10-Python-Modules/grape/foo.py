16b5219db7ed6ecff078e8a053e9c976221c7d76
2Nony3 <anthonymsawbaa@hotmail.com>
2024-11-25 17:13:20 -0500

    Update

---
+++ 10-Python-Modules/grape/foo.py
@@ -0,0 +1,11 @@
+
+
+def up():
+    print("in grape.foo.up()")
+    
+
+def down():
+    print("in grape.foo.down()")
+    
+
+    
---
+++ 10-Python-Modules/orange/foo.py
@@ -0,0 +1,12 @@
+
+
+def up():
+    print("in orange.foo.up()")
+    
+
+def down():
+    print("in orange.foo.down()")
+    
+
+
+    
